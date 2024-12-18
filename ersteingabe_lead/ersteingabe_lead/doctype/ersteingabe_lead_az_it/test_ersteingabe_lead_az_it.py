6b59a119ed14f3f58ed45fe1b293824d3d11a08b
Marcus Wiegand <marcus.wiegand@phamos.eu>
2024-12-18 14:06:54 +0000

    DocType Ersteingabe Lead in App eingefügt

---
+++ ersteingabe_lead/ersteingabe_lead/doctype/ersteingabe_lead_az_it/test_ersteingabe_lead_az_it.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, SUT and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestErsteingabeLeadAZIT(FrappeTestCase):
+	pass
