895c32ded2b7458163b0e7d29297dfd00be285ed
Marcus Wiegand <marcus.wiegand@phamos.eu>
2025-01-08 15:32:49 +0000

    Neuer DocType ersteingabe_lead

---
+++ ersteingabe_lead/ersteingabe_lead/doctype/ersteingabe_lead/test_ersteingabe_lead.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, SUT and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestErsteingabeLead(FrappeTestCase):
+	pass
