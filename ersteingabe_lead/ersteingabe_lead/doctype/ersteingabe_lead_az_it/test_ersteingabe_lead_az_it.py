36e2ffe78a5cd0737d72a947332b42b35e280cfb
Marcus Wiegand <marcus.wiegand@phamos.eu>
2025-02-06 14:41:55 +0000

    Delete Ersteingabe AZ IT

--- ersteingabe_lead/ersteingabe_lead/doctype/ersteingabe_lead_az_it/test_ersteingabe_lead_az_it.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, SUT and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestErsteingabeLeadAZIT(FrappeTestCase):
-	pass
